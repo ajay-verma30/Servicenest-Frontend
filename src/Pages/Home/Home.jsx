2352edb56daad8a5ddcf62ce2c532b72fc0075d1
Ajay Verma <aryanav30@gmail.com>
2025-07-10 14:48:46 +0530

    Added segment sdk

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+import { Container, Button } from 'react-bootstrap'
+
+function Home() {
+  return (
+    <>
+        <div className="main-container">
+            <Container>
+                
+            </Container>
+        </div>
+    </>
+  )
+}
+
+export default Home
